197d7baf116116f7455b05a7fca4ce341f185380
Dila Gurung <dila@vurung.com>
2020-12-01 18:44:39 +0545

    1s push

--- gateway/src/main/java/com/lite/ms/gateway/MsGatewayApplication.java
+++ gateway/src/main/java/com/lite/ms/gateway/MsGatewayApplication.java
@@ -4,6 +4,9 @@ import com.lite.ms.gateway.filter.ErrorFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +14,7 @@ import org.springframework.context.annotation.Bean;
 @EnableZuulProxy
 @SpringBootApplication
 @EnableEurekaServer
-public class MsGatewayApplication {
+public class MsGatewayApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MsGatewayApplication.class, args);
@@ -22,4 +25,8 @@ public class MsGatewayApplication {
     }
 
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }
 }
